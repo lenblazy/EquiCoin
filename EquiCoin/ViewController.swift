3c839b775065488ca849b0b9dcda9fcadd157c61
Lennox Mwabonje <lenblazy@gmail.com>
2025-04-27 07:05:08 +0300

    Initial Commit

---
+++ EquiCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EquiCoin
+//
+//  Created by Lennox Mwabonje on 27/04/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
