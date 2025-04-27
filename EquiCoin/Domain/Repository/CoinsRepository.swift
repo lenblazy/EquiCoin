4e42e10c89c64ff70f554e523a780c828f071fb9
Lennox Mwabonje <lenblazy@gmail.com>
2025-04-27 13:53:03 +0300

    Add project structure

---
+++ EquiCoin/Domain/Repository/CoinsRepository.swift
@@ -0,0 +1,13 @@
+//
+//  CoinsRepository.swift
+//  EquiCoin
+//
+//  Created by Lennox Mwabonje on 27/04/2025.
+//
+
+import Foundation
+
+protocol CoinsRepository {
+    
+    
+}
---
+++ EquiCoin/Presentation/Coins/CoinsVM.swift
@@ -0,0 +1,7 @@
+//
+//  CoinListViewModel.swift
+//  EquiCoin
+//
+//  Created by Lennox Mwabonje on 27/04/2025.
+//
+
