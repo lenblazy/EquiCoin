b05356d65c2dea3a9896063e58482cd135c60387
Lennox Mwabonje <lenblazy@gmail.com>
2025-04-27 16:07:41 +0300

    Create AppColors Constant

---
+++ EquiCoin/Core/Utilities/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  EquiCoin
+//
+//  Created by Lennox Mwabonje on 27/04/2025.
+//
+
+import UIKit
+
+enum AppColors {
+    static let black        = UIColor(named: "Black")!
+    static let error        = UIColor(named: "Error")!
+    static let grayLight    = UIColor(named: "GrayLight")!
+    static let grayDark     = UIColor(named: "GrayDark")!
+    static let primary      = UIColor(named: "Primary")!
+    static let primaryDark  = UIColor(named: "PrimaryDark")!
+    static let success      = UIColor(named: "Success")!
+    static let white        = UIColor(named: "White")!
+}
